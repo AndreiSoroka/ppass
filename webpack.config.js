ca164b78725e23fd2af74042b952309d56dd94b6
andreisoroka <unickpro@gmail.com>
2021-03-14 00:51:33 +0200

    added BannerPlugin to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+const { BannerPlugin } = require("webpack");
 const { resolve } = require("path");
+
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
   entry: './src/cli.js',
-  target : 'node',
+  target: 'node',
   output: {
     path: resolve(__dirname, 'dist/cli'),
     filename: 'cli.bundle.js',
   },
+  plugins: [
+    new BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
+  ]
 };
